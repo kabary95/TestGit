f02944d46830f55320b5320d504f5ec66a7a0b12
kabary95 <abdalrhmanhassan@hotmail.com>
2022-05-28 19:44:58 +0200

    TestGit

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+ * File:   main.c
+ * Author: user
+ *
+ * Created on May 27, 2022, 6:17 PM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+        
+        
+    }
+}
